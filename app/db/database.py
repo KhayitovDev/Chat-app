e2ca9b4f929b33b34a6f0d1cb4664e4a39cfded7
Khayitboy <khayitovkhayitboy@gmail.com>
2024-09-14 16:52:42 +0500

    database configuration

---
+++ app/db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = ""
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
+Base = declarative_base()
+
+class DBContext:
+    def __init__(self) -> None:
+        self.db = SessionLocal()
+    
+    def __enter__(self):
+        return self.db
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
+        
+def get_db():
+    with DBContext() as db:
+        yield db
